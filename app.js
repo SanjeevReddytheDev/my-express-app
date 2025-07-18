e36101bfb796aa964bc7593c08150b504509d38e
Sanjeev Reddy <sanjeevbforwork@gmail.com>
2025-07-18 09:19:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+  res.send('✅ Node.js App Deployed via GCP Cloud Deploy!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
